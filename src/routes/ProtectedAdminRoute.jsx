73d45572e03c97cc246822f2b8c679937b3cdf14
krishnapalpanwar24 <krishnapal.s.panwar24@gmail.com>
2026-06-23 18:56:17 +0530

    last update of the day

--- src/routes/ProtectedAdminRoute.jsx
+++ src/routes/ProtectedAdminRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 // if admin not login than send 
-// admin to login page 
+// admin to login pages
 export default function ProtectedAdminRoute({ children }) {
   const adminAuth = localStorage.getItem("adminAuth");
   return adminAuth ? children : <Navigate to="/admin-login" />;
